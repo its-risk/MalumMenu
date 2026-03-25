5aa25fdc7b000e03159c3b61ddee766638280bda
scp222thj <82206202+scp222thj@users.noreply.github.com>
2026-03-25 17:42:58 +0100

    fix(patches): fix textbox input occasionally blocking
    - seemingly caused by _currentCharPos sometimes exceeding text length without being reset, causing IndexOutOfRange exceptions

--- src/Patches/TextBoxTMPPatches.cs
+++ src/Patches/TextBoxTMPPatches.cs
@@ -71,9 +71,11 @@ public static class TextBoxTMP_IsCharAllowed
         // Get character that is currently being checked by keeping track
         // of each TextBoxTMP.IsCharAllowed call made within the foreach loop
 
+        _currentCharPos = Mathf.Clamp(_currentCharPos, 0, text.Length - 1);
+
         char currentChar = text[_currentCharPos];
 
-        if (_currentCharPos == text.Length - 1)
+        if (_currentCharPos >= text.Length - 1)
         {
             _currentCharPos = 0; // Reset position when loop finishes
         }
